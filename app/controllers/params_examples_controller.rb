a8bd767e0e95955dd800c2ed1b7c74ae337b83cb
Peter Jang <peter.x.jang@gmail.com>
2017-03-08 19:46:10 -0600

    Add solution to query name exercise

--- app/controllers/params_examples_controller.rb
+++ app/controllers/params_examples_controller.rb
@@ -4,4 +4,14 @@ class ParamsExamplesController < ApplicationController
     @my_other_message = params["another_message"]
     render "query_params.html.erb"
   end
+
+  def name_method
+    @name = params["query_name"].upcase
+    if @name.start_with?("A")
+      @name_message = "Hey your name starts with A!"
+    else
+      @name_message = ""
+    end
+    render "name.html.erb"
+  end
 end
